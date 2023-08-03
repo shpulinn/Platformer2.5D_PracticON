766770d8fdf14b23cd0df8cf96015ee2b192b119
Nikita <shpulinn@mail.ru>
2023-08-03 16:15:29 +0200

    MakeDamageOnEnter script created

---
+++ Assets/Scripts/MakeDamageOnEnter.cs
@@ -0,0 +1,44 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MakeDamageOnEnter : MonoBehaviour
+{
+    
+   [SerializeField] private int damageValue = 1;
+
+   private void OnTriggerStay(Collider other)
+   {
+       Touch(other);
+   }
+
+   private void OnCollisionStay(Collision collisionInfo)
+   {
+       Touch(collisionInfo.collider);
+   }
+
+   private void OnCollisionEnter(Collision collision)
+   {
+       Touch(collision.collider);
+   }
+
+   private void Damaged(PlayerHealth playerHealth)
+    {
+        playerHealth.TakeDamage(damageValue);
+    }
+
+    private void Touch (Collider collider) 
+    {
+        if(collider.attachedRigidbody) 
+        {
+            PlayerHealth playerHealth = collider.attachedRigidbody.GetComponent<PlayerHealth>();
+            SwordCollider swordCollider = collider.GetComponent<SwordCollider>();
+            
+            if (playerHealth && swordCollider == false)
+            {
+                Damaged(playerHealth);
+            }
+        }
+    }
+}
