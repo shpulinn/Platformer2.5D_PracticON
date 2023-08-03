5ae81fd412ce1dcfa00272728de7cabe42253488
Nikita <shpulinn@mail.ru>
2023-08-03 16:16:31 +0200

    ThrowableCreator script created

---
+++ Assets/Scripts/ThrowableCreator.cs
@@ -0,0 +1,42 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThrowableCreator : MonoBehaviour
+{
+    [SerializeField] private GameObject fakeProjectileInHand;
+    [SerializeField] private GameObject projectilePrefab;
+    [SerializeField] private Transform spawnPoint;
+    [SerializeField] private float throwForce = 10f;
+
+    private Rigidbody _rb;
+    private Transform _playerTransform;
+
+    private void Start()
+    {
+        _playerTransform = FindObjectOfType<PlayerMovement>().transform;
+    }
+
+    public void SpawnProjectile()
+    {
+        HideFakeProjectileInHand();
+        GameObject projectile = Instantiate(projectilePrefab, spawnPoint.position, spawnPoint.rotation);
+        _rb = projectile.GetComponent<Rigidbody>();
+
+        Vector3 toPlayer = (_playerTransform.position - transform.position).normalized;
+        float distanceToPlayer = Vector3.Distance(transform.position,_playerTransform.position);
+        throwForce = distanceToPlayer > 7 ? 10f : 3f;
+        _rb.velocity = toPlayer * throwForce;
+    }
+
+    public void ShowFakeProjectileInHand()
+    {
+        fakeProjectileInHand.SetActive(true);
+    }
+
+    public void HideFakeProjectileInHand()
+    {
+        fakeProjectileInHand.SetActive(false);
+    }
+}
