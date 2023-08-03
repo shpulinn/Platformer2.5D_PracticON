cac692ad27f20141b0655611a704dc0fe4eb626b
Nikita <shpulinn@mail.ru>
2023-08-03 16:16:51 +0200

    Update PlayerRotation.cs

--- Assets/Scripts/PlayerRotation.cs
+++ Assets/Scripts/PlayerRotation.cs
@@ -6,19 +6,19 @@ public class PlayerRotation : MonoBehaviour
 {
     [SerializeField] private GameInput gameInput;
     [SerializeField] private float rotationSpeed;
-    private float _xEuler = 90;
+    private float _xEuler = 0;
 
     private void Update ()
     {
         float _xAxis = gameInput.GetMovementDirectionNormalized().x;
         if (_xAxis > 0)
         {
-            _xEuler = 90f;
+            _xEuler = 0f;
 
         }
         else if (_xAxis < 0)
         {
-            _xEuler = 270f;
+            _xEuler = 180f;
         }
         transform.localRotation = Quaternion.Lerp(transform.localRotation, Quaternion.Euler(0, _xEuler, 0), Time.deltaTime * rotationSpeed);
     }
