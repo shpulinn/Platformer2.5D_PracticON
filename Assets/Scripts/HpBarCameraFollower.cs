aac4bf9fd5d688f4dacc3e17cc2487c63fac6cf9
Nikita <shpulinn@mail.ru>
2023-08-03 16:15:18 +0200

    HpBarCameraFollower script created

---
+++ Assets/Scripts/HpBarCameraFollower.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class HpBarCameraFollower : MonoBehaviour
+{
+    private Camera _camera;
+
+    private void Awake()
+    {
+        _camera = Camera.main;
+    }
+
+    private void LateUpdate()
+    {
+        transform.LookAt(new Vector3(transform.position.x, _camera.transform.position.y, _camera.transform.position.z));
+        transform.Rotate(0, 180, 0);
+    }
+}
