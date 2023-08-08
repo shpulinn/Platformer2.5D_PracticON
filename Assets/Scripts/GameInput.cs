57bfcfc7b6a7c911588298154af9d7eb0db5c5a5
Nikita <shpulinn@mail.ru>
2023-08-08 12:45:51 +0200

    Update GameInput.cs

--- Assets/Scripts/GameInput.cs
+++ Assets/Scripts/GameInput.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -12,6 +13,10 @@ public class GameInput : MonoBehaviour
     
     public event EventHandler OnJumpAction;
     public event EventHandler OnPauseAction;
+    public event EventHandler OnAttackAction;
+    public event EventHandler OnBlockActionPerformed;
+
+    public event EventHandler OnBlockActionCanceled;
     
     private void Awake()
     {
@@ -22,6 +27,27 @@ public class GameInput : MonoBehaviour
         _playerInputActions.Player.Jump.performed += Jump_Performed;
         
         _playerInputActions.Player.Pause.performed += Pause_Performed;
+        
+        _playerInputActions.Player.Attack.performed += Attack_Performed;
+        
+        _playerInputActions.Player.Block.performed += Block_Performed;
+        
+        _playerInputActions.Player.Block.canceled += Block_Canceled;
+    }
+
+    private void Block_Canceled(InputAction.CallbackContext obj)
+    {
+        OnBlockActionCanceled?.Invoke(this, EventArgs.Empty);
+    }
+
+    private void Block_Performed(InputAction.CallbackContext obj)
+    {
+        OnBlockActionPerformed?.Invoke(this, EventArgs.Empty);
+    }
+
+    private void Attack_Performed(InputAction.CallbackContext obj)
+    {
+        OnAttackAction?.Invoke(this, EventArgs.Empty);
     }
 
     private void OnDisable()
@@ -29,6 +55,12 @@ public class GameInput : MonoBehaviour
         _playerInputActions.Player.Jump.performed -= Jump_Performed;
         
         _playerInputActions.Player.Pause.performed -= Pause_Performed;
+        
+        _playerInputActions.Player.Attack.performed -= Attack_Performed;
+        
+        _playerInputActions.Player.Block.performed -= Block_Performed;
+        
+        _playerInputActions.Player.Block.canceled -= Block_Canceled;
     }
 
     private void Pause_Performed(InputAction.CallbackContext obj)
